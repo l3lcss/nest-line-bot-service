a2f2ccaa4e2cbe0f6d72d2e4bbd84cd20b274326
princel3lcS <princeboy__@hotmail.com>
2019-11-29 16:34:46 +0700

    testing deploy with heroku

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
+import 'dotenv/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  await app.listen(process.env.PORT || 9876)
 }
 
 // tslint:disable-next-line: no-floating-promises
