2640a7942197ee35bce4c9780c07da3e2b37c10d
princel3lcS <princeboy__@hotmail.com>
2019-11-29 17:07:59 +0700

    Fixed: server response

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!'
+  getHello() {
+    return {
+      name: 'LINE_MEOW_BOT_SERVICE',
+      timestamp: new Date()
+    }
   }
 }
