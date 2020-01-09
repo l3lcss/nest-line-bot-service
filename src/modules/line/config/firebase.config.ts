adb45a9af52a81537b97f3f16e5dfd74db5aa5fa
princel3lcS <princeboy__@hotmail.com>
2020-01-09 12:46:41 +0700

    feature: init firebase app and handle contact admin event

---
+++ src/modules/line/config/firebase.config.ts
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase'
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
+}
+
+const app = firebase.initializeApp(firebaseConfig)
+const db = app.database(process.env.DATABASE_URL)
+
+export default db
--- src/modules/line/line-handler/message/types/text-context/contact-admin.context.ts
+++ src/modules/line/line-handler/message/types/text-context/contact-admin.context.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@nestjs/common'
 import client from '../../../../config/line.config'
 import { MessageEventPayload } from '../../../../interfaces/line.interface'
+import db from '../../../../config/firebase.config'
+import { RESPONSE_MODE, REPLY_MESSAGE_TYPE } from '../../../../const/line.const'
 
 @Injectable()
 export class ContactAdminContext {
   async handleByMessageContext(messageEvent: MessageEventPayload): Promise<any> {
     const { replyToken } = messageEvent
 
+    await db.ref(`users/${messageEvent.source.userId}/messageEvents`).push(messageEvent)
+    await db.ref(`users/${messageEvent.source.userId}`).update({
+      responseMode: RESPONSE_MODE.LIVE_CHAT
+    })
+
     return client.replyMessage(replyToken, {
-      type: 'text',
+      type: REPLY_MESSAGE_TYPE.TEXT,
       text: `Contacting admin...\nWaiting for queue...\nCheck your queue here\n⏱⏱⏱⏱⏱⏱⏱\nline://app/1653595847-nmVaNlX7`
     })
   }
